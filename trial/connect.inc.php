303cc2b0b11753055360d7fb5f9774b5b3fadca6
Sree Harsha M <mavharsha@users.noreply.github.com>
2015-06-16 10:39:38 -0700

    Update connect.inc.php

--- trial/connect.inc.php
+++ trial/connect.inc.php
@@ -3,7 +3,7 @@
 $mysql_error='Connection error!';
 $mysql_host='localhost';
 $mysql_user='root';
-$mysql_pass='harsha';
+$mysql_pass='******';
 $mysql_db='namedate';
 
 if(!mysql_connect($mysql_host,$mysql_user,$mysql_pass)||!mysql_select_db($mysql_db))
